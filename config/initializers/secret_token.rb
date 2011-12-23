336519038055fa4663383673d40acbbdce471c55
Andrea Salicetti <andrea.salicetti@gmail.com>
2011-12-23 12:26:52 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vision::Application.config.secret_token = 'c6f6eccefb023a27e30ff71c5c1a0f1ab2a8d3c3204ecff6c28afa78bd948ff696dd1e47d6b8f229e0f3be99cdb99d20406d318e7010b7f7791df5672b97f421'
